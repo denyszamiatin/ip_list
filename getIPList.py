e195bea379b4965da32280448121854fc18fbb50
askrypny <andrii.skrypnyk@lifecell.com.ua>
2016-12-01 11:46:22 +0200

    v1

---
+++ getIPList.py
@@ -0,0 +1,27 @@
+"""
+getIPList.py
+    Gets list of ip from https://www.colocall.net/uaix/ returns it
+
+Usage:
+
+"""
+
+import urllib2
+import re
+
+url = 'https://www.colocall.net/uaix/'
+
+ip_pattern = re.compile (r'(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}')
+
+def getIP(url):
+  req = urllib2.Request(url)
+  response = urllib2.urlopen(req)
+  html_page = response.read()
+  iplist = ip_pattern.findall(html_page)
+  return iplist
+
+def main():
+  getIP(url)
+
+if __name__ == "__main__":
+  main()
