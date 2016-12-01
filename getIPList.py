cafc25295e5d9429f90d1fb3f7d162140102b03c
askrypny <andrii.skrypnyk@lifecell.com.ua>
2016-12-01 14:07:08 +0200

    v1.1 commit local_apache global "url" var changed to "site" var

--- getIPList.py
+++ getIPList.py
@@ -8,20 +8,33 @@ Usage:
 
 import urllib2
 import re
+import socket
 
-url = 'https://www.colocall.net/uaix/'
+# timeout in seconds
+timeout = 15
+socket.setdefaulttimeout(timeout)
 
 ip_pattern = re.compile (r'(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}')
 
-def getIP(url):
-  req = urllib2.Request(url)
+def getIP(site):
+  req = urllib2.Request(site)
   response = urllib2.urlopen(req)
   html_page = response.read()
   iplist = ip_pattern.findall(html_page)
   return iplist
 
 def main():
-  getIP(url)
+  if socket.getfqdn(socket.gethostname()).split(".")[-2] == 'astelit':
+    # Proxy settings
+    proxy_support = urllib2.ProxyHandler({"http": "http://proxy.astelit.ukr:3128",\
+                                          "https": "http://proxy.astelit.ukr:3128"} )
+    opener = urllib2.build_opener(proxy_support)
+    urllib2.install_opener(opener)
+
+  url = 'https://www.colocall.net/uaix/'
+  iplist = getIP(url)
+  for ip in iplist:
+   print ip
 
 if __name__ == "__main__":
   main()
