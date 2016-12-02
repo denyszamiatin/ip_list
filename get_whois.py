498092d28f259d22fb88fad96e3fbab10014d9ad
user <asdf@test.com>
2016-12-02 19:51:47 +0200

    to function

--- get_whois.py
+++ get_whois.py
@@ -1,18 +1,15 @@
 import re
 import urllib2
 
-
 def get_whois(url):
     url = 'https://hostmaster.ua/whois.php?domain=' + url
     request = urllib2.Request(url)
     response = urllib2.urlopen(request)
     html_page = response.read()
-
     r = re.findall(r'<td.*>(.*)<\/td><td>(.*)<\/td>', html_page)
+    return r
 
-    print r
+list = get_whois('lifecell.ua')
 
-    for i in r:
-        print i[0] + ':' + i[1]
-
-get_whois('lifecell.ua')
+for item in list:
+    print item[0] + ':' + item[1]
