942120e5c00570ebef6701b3ce1df866f4c95cd6
user <asdf@test.com>
2016-12-02 20:04:21 +0200

    add whois by ip

--- get_whois.py
+++ get_whois.py
@@ -3,7 +3,7 @@
 import re
 import urllib2
 
-def get_whois_data_list(domain):
+def get_whois_domain_data_list(domain):
     url = 'https://hostmaster.ua/whois.php?domain=' + domain
     request = urllib2.Request(url)
     response = urllib2.urlopen(request)
@@ -11,10 +11,31 @@ def get_whois_data_list(domain):
 
     return re.findall(r'<td.*>(.*)<\/td><td>(.*)<\/td>', html_page)
 
+def get_whois_ip_data_list(ip):
+    url = 'https://www.colocall.net/uaix/?whois=' + ip
+    request = urllib2.Request(url)
+    response = urllib2.urlopen(request)
+    html_page = response.read()
+
+    return re.findall(r'(.*):\s+(.*)', html_page)
+
+
+
+
 
 if __name__ == "__main__":
-    list = get_whois_data_list('lifecell.ua')
+    list1 = get_whois_domain_data_list('lifecell.ua')
 
     for item in list:
         print item[0] + ':' + item[1]
+
+    print (dir(list1))
+
+    list = get_whois_ip_data_list('1.179.172.0')
+
+    for item in list:
+        print item[0] + ':' + item[1]
+
+    print (dir(list2))
+
 
