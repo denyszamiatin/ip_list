f3cc3ddb836a7bbec509d10cdcf284f7d25a682c
askrypny <andrii.skrypnyk@lifecell.com.ua>
2016-12-04 20:04:51 +0200

    code changed

--- ipdb.py
+++ ipdb.py
@@ -11,7 +11,7 @@ Usage:
 """
 
 import csv
-from getIPList import _getip
+from get_ip_list import get_content, parse_ip
 from sys import argv
 from time import strftime
 from os import path, listdir
@@ -20,19 +20,11 @@ from os import path, listdir
 DB_PATH = r'.\ipdb'
 
 
-def _getdb():
-  ip_db_latest = path.join(DB_PATH, _getlatestdbfile())
-  with open(ip_db_latest, 'r') as csv_file:
-    reader = csv.reader(csv_file, delimiter=',')
-    for line in reader:
-      print line
-
-
-def _getlatestdbfile():
+def _get_latest_db_file():
    return sorted(listdir(DB_PATH))[-1]
 
 
-def updatedb(ip_list):
+def update_db(ip_list):
   ip_db_new = path.join(DB_PATH, r'iplist.' + strftime("%Y%m%d%H%M%S") + '.csv')
   with open(ip_db_new, 'w') as csv_file:
     writer = csv.writer(csv_file, delimiter=',', lineterminator='\n', escapechar='|', quoting=csv.QUOTE_NONE)
@@ -40,10 +32,11 @@ def updatedb(ip_list):
     for ip in ip_list:
       ip, pref = ip.split('/')
       writer.writerow([ip, pref])
+  return ip_db_new
 
 
-def searchDB(ip='.'):
-  ip_db_srch = path.join(DB_PATH, _getlatestdbfile())
+def search_in_db(ip='.'):
+  ip_db_srch = path.join(DB_PATH, _get_latest_db_file())
   with open(ip_db_srch, 'r') as csv_file:
     reader = csv.reader(csv_file, delimiter=',')
     next(reader, None)
@@ -55,11 +48,9 @@ if __name__ == "__main__":
   try:
     url = 'https://www.colocall.net/uaix/prefixes.txt'
     if argv[1] == 'up':
-        updatedb(_getip(url)[0:10])
-        print searchDB()
-    else:
-      print "Main"
+      update_db(parse_ip(get_content(url))[0:20])
+      print search_in_db('2')
   except IOError as er:
     print "I/O error({0}): {1}".format(er.errno, er.strerror)
-  except IndexError:
-    print IndexError.errno
+  except IndexError as er:
+    print "I/O error({0}): {1}".format(er.errno, er.strerror)
