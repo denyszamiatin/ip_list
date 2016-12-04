fa61293e992bdae1486c2222bb053aec0a401c3e
askrypny <andrii.skrypnyk@lifecell.com.ua>
2016-12-04 13:05:41 +0200

    commit changes

--- ipdb.py
+++ ipdb.py
@@ -11,7 +11,7 @@ Usage:
 """
 
 import csv
-from getIPList import getip
+from getIPList import _getip
 from sys import argv
 from time import strftime
 from os import path, listdir
@@ -19,11 +19,11 @@ from os import path, listdir
 
 file_name = r'iplist.' + strftime("%Y%m%d%H%M%S") + '.csv'
 db_path = r'.\ipdb'
-ip_db = path.join(db_path, file_name)
 
 
 def _getdb():
-  with open(ip_db, 'r') as csv_file:
+  ip_db_latest = path.join(db_path, _getlatestdbfile())
+  with open(ip_db_latest, 'r') as csv_file:
     reader = csv.reader(csv_file, delimiter=',')
     for line in reader:
       print line
@@ -34,7 +34,8 @@ def _getlatestdbfile():
 
 
 def updatedb(ip_list):
-  with open(path.join(db_path, r'iplist.' + strftime("%Y%m%d%H%M%S") + '.csv'), 'w') as csv_file:
+  ip_db_new = path.join(db_path, r'iplist.' + strftime("%Y%m%d%H%M%S") + '.csv')
+  with open(ip_db_new, 'w') as csv_file:
     writer = csv.writer(csv_file, delimiter=',', lineterminator='\n', escapechar='|', quoting=csv.QUOTE_NONE)
     writer.writerow(['IP', 'Prefix'])
     for ip in ip_list:
@@ -43,7 +44,8 @@ def updatedb(ip_list):
 
 
 def searchDB(ip='.'):
-  with open(path.join(db_path, _getlatestdbfile()), 'r') as csv_file:
+  ip_db_srch = path.join(db_path, _getlatestdbfile())
+  with open(ip_db_srch, 'r') as csv_file:
     reader = csv.reader(csv_file, delimiter=',')
     next(reader, None)
     for line in reader:
@@ -55,22 +57,15 @@ def searchDB(ip='.'):
 #  return iplist
 
 
-def main():
-  url = 'http://192.168.56.102/uaix.html'
+if __name__ == "__main__":
   try:
+    url = 'https://www.colocall.net/uaix/prefixes.txt'
     if argv[1] == 'up':
-      updatedb(getip(url)[0:10])
-      searchDB()
-    #      _getdb()
+        updatedb(_getip(url)[0:10])
+        searchDB()
     else:
       print "Main"
   except IOError as er:
-    print er
-    # print "I/O error({0}): {1}".format(e.errno, e.strerror)
-
-
-if __name__ == "__main__":
-  try:
-    main()
+    print "I/O error({0}): {1}".format(er.errno, er.strerror)
   except IndexError:
     print IndexError.errno
